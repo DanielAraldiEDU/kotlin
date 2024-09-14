6d63802b2ad542e677e8af0f7d6c65ee35eb3708
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-09-13 21:36:38 -0300

    feat: add some exercises

---
+++ 13-09-2024/exercise-one/main.kt
@@ -0,0 +1,27 @@
+fun isOdd(number: Int): Boolean {
+    return number % 2 == 1
+}
+
+fun isPrime(number: Int): Boolean {
+    if (number < 2) return false
+    
+    val sqrtValue = Math.sqrt(number.toDouble()).toInt()
+    for (index in 2..sqrtValue) {
+        if (number % index == 0) return false
+    }
+    
+    return true
+}
+
+
+fun main() {
+    println("Hello, world!!!")
+    
+    for (number in 1..20) {
+        println("$number is odd? ${isOdd(number)}")
+        println("$number is prime? ${isPrime(number)}")
+    }
+    
+    val countries = listOf("Brazil", "Germany", "Peru", "Russia", "Uruguai", "Canada")
+    countries.forEach{ print("$it ") }
+}
